b6cbad76f8a9407d6a3a7306e4980f45a617a5c3
Raúl Pardo Zurita <raul.pardoz@gmail.com>
2022-11-01 14:33:03 -0300

    Commit inicial

---
+++ src/main/java/cl/bahatech/bahagamesbackend/BahaGamesBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.bahatech.bahagamesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahaGamesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BahaGamesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/bahatech/bahagamesbackend/BahaGamesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bahatech.bahagamesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahaGamesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
